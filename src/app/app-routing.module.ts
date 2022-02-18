90528a90324899ca0a2476498581cb3a10c9c63c
Sebastian <sebastiansegovia80@hotmail.com>
2022-02-18 11:22:31 -0300

    Creacion de barra de Menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './componentes/menu/menu.component';
+import { PrincipalComponent } from './componentes/principal/principal.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: PrincipalComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
