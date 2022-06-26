dae4beaf78369c2af16aa263281a515fe8d6e15a
Sebastian <sebastiansegovia80@hotmail.com>
2022-06-26 11:38:54 -0300

    Modal de Edicion de Educacion y funcionalidad a boton eliminar Educacion

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -1,7 +1,9 @@
 
 
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
 import { Educacion } from 'src/app/models/educacion';
 import { EducacionServiceService } from 'src/app/servicios/educacion-service.service';
 
@@ -15,6 +17,7 @@ export class EducacionComponent implements OnInit {
   public listaEducacion : Educacion[]=[];
   public altaEducacion : FormGroup | any;
   private eduModificar: Educacion | undefined;
+  public esNuevo :boolean = true;
 
 
   constructor(private educacionService:EducacionServiceService, private formBuilder : FormBuilder) { }
@@ -25,7 +28,7 @@ export class EducacionComponent implements OnInit {
     
     
     }
-
+    /* Metodo para mostrar Educacion desde la Api */
     public mostrarEducacion(){
       this.educacionService.getEducacion().subscribe(
         data =>{this.listaEducacion=data
@@ -33,8 +36,10 @@ export class EducacionComponent implements OnInit {
       });
     }
     
+    /* Metodo para iniciar Formulario */
     public agregarEducacion(){
       this.altaEducacion=this.formBuilder.group({ 
+        id: '',
         titulo: '',
         institucion: '',
         anioInicio: '',
@@ -42,6 +47,8 @@ export class EducacionComponent implements OnInit {
         detalle: '',
         urlImagen: ''})
     }
+
+    /* Metodo para dar de Alta Educacion en Base de datos usando la Api */
     public agregarEducacionCargada(){
       const educacion=this.altaEducacion.value;
       this.educacionService.crearEducacion(educacion).subscribe(
@@ -55,7 +62,38 @@ export class EducacionComponent implements OnInit {
     
     public editarEducacion(educacion :Educacion){
       this.eduModificar=educacion;
+      this.altaEducacion.setValue({
+        id:educacion.id,
+        titulo: educacion.titulo,
+        institucion: educacion.institucion,
+        anioInicio: educacion.anioInicio,
+        anioFin: educacion.anioFin,
+        detalle: educacion.detalle,
+        urlImagen: educacion.urlImagen
+      })
+      this.esNuevo=false
+      /*this.educacionService.modificarEducacion()*/
     }
+
+    public modificarEducacionCargada(){
+      const educacion=this.altaEducacion.value;
+      this.educacionService.modificarEducacion(educacion).subscribe(
+        data=>{
+        this.mostrarEducacion();
+        this.altaEducacion.reset();
+        });
+        this.esNuevo=true;
+    }
+
+    public eliminarEducacion(id: number){
+      this.educacionService.borrarEducacion(id).subscribe(
+        data=>{
+          this.mostrarEducacion();
+        }
+      )
+    }
+
+
    
 }
 
